0bb409ab3837669b3cdf35567569a76d1918a977
AhmedSaadawy10 <saadawyahmed42@gmail.com>
2024-04-29 00:01:39 +0300

    day4

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,10 @@
     "author": "Ahmed Saadawy",
     "description": "The ITI BNS module for managing students data and courses",
     "data": [
+        "reports/iti_students_template.xml",
+        "reports/iti_reports.xml",
+        "security/iti_security_view.xml",
+        "security/ir.model.access.csv",
         "views/track_view.xml",
         "views/student_view.xml",
     ]
